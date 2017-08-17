5ab0b9da25a8bb7fcfe6c2834d93c04eaafac5cd
Markus Felten <Markus.Felten@gmx.de>
2017-08-18 00:09:30 +0200

    test: WhitespaceIgnoreToken lineNumber

--- tests/whitespace-ignore-token-test.js
+++ tests/whitespace-ignore-token-test.js
@@ -3,6 +3,10 @@ import WhitespaceIgnoreToken from '../src/whitespace-ignore-token';
 import { tokenTester } from './util';
 
 test('whitespace token', async t => {
-  const { tokens, tts } = await tokenTester(WhitespaceIgnoreToken, [' ', ' ']);
-  t.is(tts.lineNumber, 1);
+  const { tokens, tts } = await tokenTester(WhitespaceIgnoreToken, [
+    ' ',
+    '\n',
+    ' '
+  ]);
+  t.is(tts.lineNumber, 2);
 });
