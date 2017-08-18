777c47e3f9bc2fd339d51d34bd8e6ac892025fb4
Markus Felten <Markus.Felten@gmx.de>
2017-08-19 01:21:30 +0200

    test: larger numbers

--- tests/number-token-test.js
+++ tests/number-token-test.js
@@ -3,15 +3,15 @@ import NumberToken from '../src/number-token';
 import { tokenTester } from './util';
 
 test('number token', async t => {
-  const { tokens, tts } = await tokenTester(NumberToken, ['1']);
+  const { tokens, tts } = await tokenTester(NumberToken, ['17']);
 
-  t.is(tokens[0].value, 1);
+  t.is(tokens[0].value, 17);
   t.is(tts.lineNumber, 1);
 });
 
 test.skip('number token over several chunks', async t => {
-  const { tokens, tts } = await tokenTester(NumberToken, ['1', '2']);
+  const { tokens, tts } = await tokenTester(NumberToken, ['17', '2']);
 
-  t.is(tokens[0].value, 12);
+  t.is(tokens[0].value, 172);
   t.is(tts.lineNumber, 1);
 });
